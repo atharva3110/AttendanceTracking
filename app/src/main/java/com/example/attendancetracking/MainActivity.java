0b966bda84c38c6bc208fa5d8a92aaf69e866819
atharva3110 <47656352+atharva3110@users.noreply.github.com>
2019-12-03 15:08:19 +0530

    first

---
+++ app/src/main/java/com/example/attendancetracking/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.attendancetracking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button startBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startBtn = (Button) findViewById(R.id.start_btn);
+
+        startBtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLogin();
+            }
+        });
+    }
+    
+
+    public void openLogin() {
+        Intent intent = new Intent(this, login.class);
+        startActivity(intent);
+    }
+}
