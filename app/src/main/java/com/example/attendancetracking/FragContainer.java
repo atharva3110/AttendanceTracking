93c6b70b48c92ee446e1f2ade9c31e4d198209c2
vedpatil611 <vedpatil611@gmail.com>
2019-12-12 17:22:04 +0530

    Login layout fix

--- app/src/main/java/com/example/attendancetracking/FragContainer.java
+++ app/src/main/java/com/example/attendancetracking/FragContainer.java
@@ -1,7 +1,15 @@
 package com.example.attendancetracking;
 
+import android.app.LauncherActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,7 +20,9 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class FragContainer extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+import java.util.ArrayList;
+
+public class FragContainer extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, TimeTableUploader.TimeTableUploaderListner {
 
     private DrawerLayout drawerLayout;
     private Toolbar toolbar;
@@ -39,12 +49,12 @@ public class FragContainer extends AppCompatActivity implements NavigationView.O
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
 
-        /*if(savedInstanceState == null) {
+        if(savedInstanceState == null) {
             getSupportFragmentManager()
                     .beginTransaction()
-                    .replace(R.id.fragment_container, //class instance)
+                    .replace(R.id.fragment_container, new TimeTableUploader())
                     .commit();
-        }*/
+        }
     }
 
     @Override
@@ -54,5 +64,76 @@ public class FragContainer extends AppCompatActivity implements NavigationView.O
                 break;
         }
         return false;
+    }
+
+    @Override
+    public void setAdapterForListView(ListView lv, BaseAdapter ba) {
+        ba = new EditTextListAdapter();
+        lv.setAdapter(ba);
+    }
+
+    public class EditTextListAdapter extends BaseAdapter {
+        private LayoutInflater layoutInflater;
+        private ArrayList<ListItem> list;
+
+        public EditTextListAdapter() {
+            super();
+            layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            list = new ArrayList<>();
+            notifyDataSetChanged();
+        }
+        public void addMoreItemToList() {
+            ListItem listItem = new ListItem();
+            listItem.caption = "Subject";
+            list.add(listItem);
+        }
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return i;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            ViewHolder holder;
+            if(view == null) {
+                holder = new ViewHolder();
+                view = layoutInflater.inflate(R.layout.subject_collector_textbox, viewGroup);
+                holder.caption = view.findViewById(R.id.textbox_id);
+                view.setTag(holder);
+            } else {
+                holder = (ViewHolder) view.getTag();
+            }
+            holder.caption.setText(list.get(i).caption);
+            holder.caption.setId(i);
+
+            holder.caption.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View v, boolean hasFocus) {
+                    if(!hasFocus) {
+                        final int pos = v.getId();
+                        final EditText cap = (EditText) v;
+                        list.get(pos).caption = cap.getText().toString();
+                    }
+                }
+            });
+
+            return view;
+        }
+    }
+    class ViewHolder {
+        EditText caption;
+    }
+    class ListItem {
+        String caption;
     }
 }
